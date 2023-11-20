e5d12b32ed5a68948cb9b056d61d26992b10dee3
Laasya Budda <laasya.b@ibm.com>
2023-11-20 09:54:38 +0530

    First commit

---
+++ controller/Main.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define(["sap/ui/core/mvc/Controller"],function(Controller)
+{
+    return Controller.extend ("com.ibm.sampleapp.Main",{
+        onInit :function() {},
+        onBeforeRendering: function() {},
+        onAfterRendering:function(){},
+        onExit: function(){},
+        });
+
+});
