2695acf5be7c1b0580047031c9933786532994d0
Laasya Budda <laasya.b@ibm.com>
2023-11-20 20:18:24 +0530

    first commit

--- controller/Main.controller.js
+++ controller/Main.controller.js
@@ -1,10 +1,21 @@
 sap.ui.define(["sap/ui/core/mvc/Controller"],function(Controller)
 {
-    return Controller.extend ("com.ibm.sampleapp.Main",{
+    return Controller.extend ("com.ibm.simpleapp.controller.Main",{
         onInit :function() {},
         onBeforeRendering: function() {},
         onAfterRendering:function(){},
         onExit: function(){},
-        });
+        onHtmlPress: function(){
+            alert("this is a Html Button");
+        },
+        onPressJSON: function(){
+            alert("this is a Json Button");
+        },
+        onPressJS: function(){
+            alert("this is a Js button");
+        },
 
+    
+        });
 });
+
